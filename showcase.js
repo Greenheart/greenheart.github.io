df50e92c3d7860b95cb290957277614899c670ba
Samuel Plumppu <greenheartmusic@gmail.com>
2016-06-07 13:02:08 +0200

    Update showcase.js
    Use arrow-functions and remove semicolons.

--- showcase.js
+++ showcase.js
@@ -1,35 +1,34 @@
-var curProject = 3,
-    projects,
-    fadeTime = 600;
+let curProject = 3
+const fadeTime = 600
 
-$(document).ready(function() {
-  projects = $('.showcase .project');
-  $(projects[curProject]).fadeIn(fadeTime);
+$(document).ready(() => {
+  const projects = $('.showcase .project')
+  $(projects[curProject]).fadeIn(fadeTime)
 
   // set up the click-event handlers
-  $('.arrow-prev').click(function() {
-    $(projects[curProject]).fadeOut(fadeTime, function() {
+  $('.arrow-prev').click(() => {
+    $(projects[curProject]).fadeOut(fadeTime, () => {
       // callback-function that enables crossfading between project-views
 
-      curProject--;
+      curProject--
       if (curProject < 0) {
         // Only loop over actual projects
-        curProject = projects.length - 1; // go to last
+        curProject = projects.length - 1 // go to last
       }
-      $(projects[curProject]).fadeIn(fadeTime);
-    });
-  });
+      $(projects[curProject]).fadeIn(fadeTime)
+    })
+  })
 
-  $('.arrow-next').click(function() {
-    $(projects[curProject]).fadeOut(fadeTime, function() {
+  $('.arrow-next').click(() => {
+    $(projects[curProject]).fadeOut(fadeTime, () => {
       // callback-function that enables crossfading between project-views
 
-      curProject++;
+      curProject++
       if (curProject >= projects.length) {
         // only loop over actual projects
-        curProject = 0; // go to first
+        curProject = 0 // go to first
       }
-      $(projects[curProject]).fadeIn(fadeTime);
-    });
-  });
-});
+      $(projects[curProject]).fadeIn(fadeTime)
+    })
+  })
+})
