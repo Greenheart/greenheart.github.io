e58875870e2418339494fe813501181a73478887
Samuel Plumppu <greenheartmusic@gmail.com>
2018-05-05 12:53:55 +0200

    Fix project showcase bug.
    This prevents the project showcase from showing multiple projects if navigation was used too quickly, while a project already is fading to be shwon.

--- src/page.js
+++ src/page.js
@@ -22,23 +22,32 @@ $(function () {
   var i = 0
   var fadeTime = 500
   var projects = $('.showcase .project')
+  var isFading = false
   $(projects[i]).fadeIn(fadeTime)
 
   $('.arrow-prev').click(function () {
-    $(projects[i]).fadeOut(fadeTime, function () {
-      if (--i < 0) {
-        i = projects.length - 1
-      }
-      $(projects[i]).fadeIn(fadeTime)
-    })
+    if (!isFading) {
+      isFading = true
+      $(projects[i]).fadeOut(fadeTime, function () {
+        if (--i < 0) {
+          i = projects.length - 1
+        }
+        $(projects[i]).fadeIn(fadeTime)
+        isFading = false
+      })
+    }
   })
 
   $('.arrow-next').click(function () {
-    $(projects[i]).fadeOut(fadeTime, function () {
-      if (++i >= projects.length) {
-        i = 0
-      }
-      $(projects[i]).fadeIn(fadeTime)
-    })
+    if (!isFading) {
+      isFading = true
+      $(projects[i]).fadeOut(fadeTime, function () {
+        if (++i >= projects.length) {
+          i = 0
+        }
+        $(projects[i]).fadeIn(fadeTime)
+        isFading = false
+      })
+    }
   })
 })
