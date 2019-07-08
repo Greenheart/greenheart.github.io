4be09b3838bfa03f64680beccf15c301b7609305
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-08 23:32:02 +0200

    Update siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`,
+        title: `Samuel Plumppu | System Developer`,
+        description: `Swedish orienteer and system developer.`,
+        author: `Samuel Plumppu`,
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
