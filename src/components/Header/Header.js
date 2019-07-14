26ecd03ba0fa6f7c5df1a56a4bd8647c89985a3f
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-14 19:43:37 +0200

    Add header logo and update styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,42 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import styles from './Header.module.css'
 
-const Header = ({ siteTitle }) => (
-    <header className={styles.header}>
-        <Link to="/">Logo</Link>
-        <Link to="/" className={styles.siteTitle}>
-            Samuel Plumppu
-        </Link>
-        <nav className={styles.mainMenu}>
-            <Link to="#about">About</Link>
-            <Link to="#projects">Projects</Link>
-            <Link to="#skills">Skills</Link>
-            <Link to="#contact">Contact</Link>
-        </nav>
-    </header>
-)
+const Header = ({ siteTitle }) => {
+    return (
+        <header className={styles.header}>
+            <StaticQuery
+                query={graphql`
+                    {
+                        compass: file(relativePath: { eq: "compass.svg" }) {
+                            publicURL
+                        }
+                    }
+                `}
+                render={data => (
+                    <Link to="/">
+                        <img
+                            src={data.compass.publicURL}
+                            alt="compass logo"
+                            width="64"
+                        />
+                    </Link>
+                )}
+            />
+
+            <Link to="/" className={styles.siteTitle}>
+                Samuel Plumppu
+            </Link>
+            <nav className={styles.mainMenu}>
+                <Link to="#about">About</Link>
+                <Link to="#projects">Projects</Link>
+                <Link to="#skills">Skills</Link>
+                <Link to="#contact">Contact</Link>
+            </nav>
+        </header>
+    )
+}
 
 export default Header
