98a7b1205c2dba7c67e434347fb2318440243983
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-14 17:55:15 +0200

    Test using gatsby-image to add WebP image

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
 import styles from './About.module.css'
 
 const About = () => {
     return (
-        <section id={styles.about}>
-            <p>About</p>
-        </section>
+        <StaticQuery
+            query={graphql`
+                {
+                    Tiomila2015: file(
+                        relativePath: { eq: "Tiomila2015-HD.jpg" }
+                    ) {
+                        childImageSharp {
+                            fluid(maxWidth: 2000) {
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
+                }
+            `}
+            render={data => (
+                <section id={styles.about}>
+                    <p>About</p>
+                    <Img
+                        fluid={data.Tiomila2015.childImageSharp.fluid}
+                        alt="Seconds after the start of orienteering relay Tiomila 2015."
+                    />
+                </section>
+            )}
+        />
     )
 }
 
