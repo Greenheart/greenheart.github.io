52196e7c29d1e291276a19cf91ff449169c70486
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-15 10:30:42 +0200

    Create Skills component. Also add support for JSON

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,14 +1,44 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import styles from './Skills.module.css'
 
 const Skills = () => {
-    // TODO: Load JSON-data for Skills.
+    const data = useStaticQuery(graphql`
+        query SkillsQuery {
+            allSkillsJson {
+                edges {
+                    node {
+                        skill
+                        level
+                    }
+                }
+            }
+        }
+    `)
+
     return (
-        <section class={styles.skills} id="skills">
-            <p>Skills</p>
+        <section id="skills">
+            <h2>Skills</h2>
+            <div className={styles.skillsContainer}>
+                {getSkills(data)
+                    .sort(byLevel)
+                    .map(Skill)}
+            </div>
         </section>
     )
 }
+
+const getSkills = data => data.allSkillsJson.edges.map(skill => skill.node)
+const byLevel = (a, b) => b.level - a.level
+
+const Skill = ({ skill, level }) => (
+    <div className={styles.skill}>
+        <span>{skill}</span>
+        <div className={styles.bar}>
+            <div className={styles.level} style={{ width: `${level * 10}%` }} />
+        </div>
+    </div>
+)
 
 export default Skills
