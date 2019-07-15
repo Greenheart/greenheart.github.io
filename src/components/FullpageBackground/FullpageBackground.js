d0830f610f812c145149e7c56a7ee044da353151
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-15 23:04:45 +0200

    Fix backgrounnd image positioning
    The problems were caused by the original image file, since it had different
    crop than the HD version, it didn't show in the same way.

--- src/components/FullpageBackground/FullpageBackground.js
+++ src/components/FullpageBackground/FullpageBackground.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
-import Img from 'gatsby-image'
 import styled from 'styled-components'
 import BackgroundImage from 'gatsby-background-image'
 
-import styles from './FullpageBackground.module.css'
+// import styles from './FullpageBackground.module.css'
 
 const BackgroundSection = ({ className }) => {
     const data = useStaticQuery(graphql`
         {
-            Tiomila2015: file(relativePath: { eq: "Tiomila2015-HD.jpg" }) {
+            Tiomila2015: file(relativePath: { eq: "Tiomila2015.jpg" }) {
                 childImageSharp {
-                    fluid(maxWidth: 2000) {
+                    fluid(quality: 100, maxWidth: 2000) {
                         ...GatsbyImageSharpFluid_withWebp
                     }
                 }
@@ -25,20 +24,21 @@ const BackgroundSection = ({ className }) => {
             fluid={imageData}
             backgroundColor={`#040e18`}
             alt="Seconds after the start of orienteering relay Tiomila 2015."
+            durationFadeIn={10}
         />
     )
 }
 
 const FullpageBackground = styled(BackgroundSection)`
     position: fixed !important;
-    top: 0;
-    left: 0;
-    z-index: -10;
-    width: 100vw;
     height: 100vh;
+    width: 100vw;
+    left: 0;
+    top: 0;
+    z-index: -100;
+    background-size: cover;
     background-position: center center;
     background-repeat: no-repeat;
-    background-size: cover;
 `
 
 export default FullpageBackground
