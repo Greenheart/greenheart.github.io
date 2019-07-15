671ce7951a0479bff641cc2723756d59010bd317
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-15 12:20:59 +0200

    Add gatsby-background-image and dependencies

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import styled from 'styled-components'
+import BackgroundImage from 'gatsby-background-image'
 
 import styles from './Intro.module.css'
 
-const Intro = () => {
+const BackgroundSection = ({ className }) => {
     const data = useStaticQuery(graphql`
         {
             Tiomila2015: file(relativePath: { eq: "Tiomila2015-HD.jpg" }) {
@@ -16,16 +18,25 @@ const Intro = () => {
             }
         }
     `)
-
+    const imageData = data.Tiomila2015.childImageSharp.fluid
     return (
-        <section id="intro">
-            <p>Intro</p>
-            <Img
-                fluid={data.Tiomila2015.childImageSharp.fluid}
+        <section id="intro" className={styles.intro}>
+            <BackgroundImage
+                className={className}
+                fluid={imageData}
+                backgroundColor={`#040e18`}
                 alt="Seconds after the start of orienteering relay Tiomila 2015."
             />
         </section>
     )
 }
+
+const Intro = styled(BackgroundSection)`
+    width: 100%;
+    height: 100%;
+    background-position: 55% 20%;
+    background-repeat: no-repeat;
+    background-size: cover;
+`
 
 export default Intro
