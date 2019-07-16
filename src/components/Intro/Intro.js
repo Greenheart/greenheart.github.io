8ef41f5e08cb53138a6f3b8aad877f4ed36f9744
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-16 17:45:06 +0200

    Improve visuals for intro section.

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -5,10 +5,17 @@ import styles from './Intro.module.css'
 const Intro = () => {
     return (
         <section id="intro" className={styles.intro}>
-            <h1>Samuel Plumppu</h1>
-            <p>Swedish orienteer and developer</p>
+            <div className={styles.introBg}>
+                <h1>Samuel Plumppu</h1>
+                <p>Swedish orienteer and developer</p>
+                <span className={classnames(styles.triangle, styles.t1)} />
+                <span className={classnames(styles.triangle, styles.t2)} />
+                <span className={classnames(styles.triangle, styles.t3)} />
+            </div>
         </section>
     )
 }
+
+const classnames = (...classes) => classes.join(' ')
 
 export default Intro
