38e40c2d389e6ff2fee342d9b056f3ff51935b58
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-24 22:51:42 +0200

    Load projects data into component & show titles.
    Added Swiper component to show projects easily.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,32 +1,59 @@
 import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Swiper from 'react-id-swiper'
+import 'react-id-swiper/lib/styles/css/swiper.css'
 
 import Triangles from '../Triangles'
 import styles from './Projects.module.css'
 
-class Projects extends React.Component {
-    constructor(props) {
-        super(props)
+const Projects = () => {
+    return (
+        <section className={styles.projects}>
+            <h2 id="projects">Projects</h2>
+            <ProjectShowcase />
+            <Triangles color="var(--orange)" direction="bottom" baseSize={30} />
+        </section>
+    )
+}
 
-        this.state = {}
+const ProjectShowcase = () => {
+    const props = {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        loop: true,
     }
 
-    componentDidMount() {
-        // TODO: fetch projects data.
-        // Then show them using a `Project`-component. `Projects` is the stateful wrapper, letting users navigate each project.
-    }
+    const data = useStaticQuery(graphql`
+        {
+            allMarkdownRemark {
+                edges {
+                    node {
+                        frontmatter {
+                            title
+                            tech
+                            github
+                            demo
+                        }
+                        html
+                    }
+                }
+            }
+        }
+    `)
 
-    render() {
-        return (
-            <section className={styles.projects}>
-                <h2 id="projects">Projects</h2>
-                <Triangles
-                    color="var(--orange)"
-                    direction="bottom"
-                    baseSize={30}
-                />
-            </section>
-        )
-    }
+    const projects = data.allMarkdownRemark.edges.map(p => p.node)
+
+    return (
+        <section className={styles.projectShowcase}>
+            <Swiper {...props}>
+                {projects.map(p => (
+                    <div key={p.frontmatter.title}>{p.frontmatter.title}</div>
+                ))}
+            </Swiper>
+        </section>
+    )
 }
 
 export default Projects
