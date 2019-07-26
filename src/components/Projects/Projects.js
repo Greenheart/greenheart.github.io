fe8b3c3503223fd15e0eed6e13b91baf85ab430c
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-26 23:50:14 +0200

    Fetch + combine project data + images.
    This format will make it easier to add or change projects in the future.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,6 +17,7 @@ const Projects = () => {
 }
 
 const ProjectShowcase = () => {
+    // Fetch project data + images.
     const data = useStaticQuery(graphql`
         {
             allMarkdownRemark {
@@ -34,25 +35,71 @@ const ProjectShowcase = () => {
                     }
                 }
             }
+
+            isoConquest: file(relativePath: { eq: "iso-conquest.png" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            tabTyper: file(relativePath: { eq: "tab-typer.png" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            theResponsible: file(relativePath: { eq: "the-responsible.png" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            primeOrNot: file(relativePath: { eq: "numbers-prime-or-not.png" }) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            goaSpaceSurvival: file(
+                relativePath: { eq: "goa-space-survival-2.png" }
+            ) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            achtungPanzer: file(
+                relativePath: { eq: "achtung_intense_battle_wow_2.png" }
+            ) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+            theNotSoBusyBusiness: file(
+                relativePath: { eq: "the-not-so-busy-business.png" }
+            ) {
+                childImageSharp {
+                    fluid(quality: 90, maxWidth: 1000) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
         }
     `)
 
     const getProjectById = (id, projects = data.allMarkdownRemark.edges) => ({
-        ...(projects.find(project => {
-            return project.node.frontmatter.id === id
-        }) || {}),
+        ...(projects.find(project => project.node.frontmatter.id === id) || {}),
     })
     const getImageById = (id, images = data) => images[id]
 
-    const projects = [
-        'iso-conquest',
-        'tab-typer',
-        'the-responsible',
-        'prime-or-not',
-        'goa-space-survival',
-        'achtung-panzer',
-        'the-not-so-busy-business',
-    ].map(id => ({
+    const projects = Object.keys(projectImages).map(id => ({
         ...getProjectById(id).node,
         img: getImageById(id),
     }))
@@ -64,6 +111,19 @@ const ProjectShowcase = () => {
             ))}
         </Carousel>
     )
+}
+
+// This object has two responsibilities:
+//   1) Set the right image for each project
+//   2) Decide the order that projects are shown. Reorder properties to change it.
+const projectImages = {
+    isoConquest: 'iso-conquest.png',
+    tabTyper: 'tab-typer.png',
+    theResponsible: 'the-responsible.png',
+    primeOrNot: 'numbers-prime-or-not.png',
+    goaSpaceSurvival: 'goa-space-survival-2.png',
+    achtungPanzer: 'achtung_intense_battle_wow_2.png',
+    theNotSoBusyBusiness: 'the-not-so-busy-business.png',
 }
 
 export default Projects
