44f5a3c80424eeb84ede621bd70ba538b6d77272
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-28 21:01:51 +0200

    Add project details sidebar to show tech + links.Surprised by how responsive CSS Grid is by default!

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Img from 'gatsby-image'
+import { IoLogoGithub, IoIosLink } from 'react-icons/io'
 
 import { getCSSVariable } from '../../utils/Helpers'
 import styles from './Project.module.css'
@@ -22,16 +23,39 @@ const Project = ({ project }) => {
                 <h3>{project.frontmatter.title}</h3>
             </div>
             <div className={styles.projectDetails}>
-                <div>
-                    <h4>Tech</h4>
+                <aside className={styles.projectSidebar}>
+                    <span className={styles.year}>
+                        {project.frontmatter.year}
+                    </span>
+
                     <ul>
                         {project.frontmatter.tech.map((tech, i) => (
                             <li key={i}>{tech}</li>
                         ))}
                     </ul>
-                    <span>{project.frontmatter.year}</span>
-                    {/* TODO: ADD github- and demo links */}
-                </div>
+                    <span className={styles.links}>
+                        {project.frontmatter.github ? (
+                            <a
+                                href={project.frontmatter.github}
+                                title="View on GitHub"
+                            >
+                                <IoLogoGithub />
+                            </a>
+                        ) : (
+                            ''
+                        )}
+                        {project.frontmatter.demo ? (
+                            <a
+                                href={project.frontmatter.demo}
+                                title="View demo"
+                            >
+                                <IoIosLink />
+                            </a>
+                        ) : (
+                            ''
+                        )}
+                    </span>
+                </aside>
                 <div
                     className="description"
                     dangerouslySetInnerHTML={{ __html: project.html }}
