bc597696408f0dea9409ae721b757126a0a426db
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-28 11:22:01 +0200

    Add basic project slide layout.

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -17,20 +17,22 @@ const Project = ({ project }) => {
             <div className={styles.projectTitle}>
                 <h3>{project.frontmatter.title}</h3>
             </div>
-            <div>
-                <h4>Tech</h4>
-                <ul>
-                    {project.frontmatter.tech.map((tech, i) => (
-                        <li key={i}>{tech}</li>
-                    ))}
-                </ul>
-                <span>{project.frontmatter.year}</span>
-                {/* TODO: ADD github- and demo links */}
+            <div className={styles.projectDetails}>
+                <div>
+                    <h4>Tech</h4>
+                    <ul>
+                        {project.frontmatter.tech.map((tech, i) => (
+                            <li key={i}>{tech}</li>
+                        ))}
+                    </ul>
+                    <span>{project.frontmatter.year}</span>
+                    {/* TODO: ADD github- and demo links */}
+                </div>
+                <div
+                    className="description"
+                    dangerouslySetInnerHTML={{ __html: project.html }}
+                />
             </div>
-            <div
-                className="description"
-                dangerouslySetInnerHTML={{ __html: project.html }}
-            />
         </article>
     )
 }
