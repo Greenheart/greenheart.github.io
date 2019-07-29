477de45a92f8eee5a20672ecdb7b1ebf63432a20
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-29 18:49:03 +0200

    Add updated About section with new content.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import { IoIosCode, IoIosLeaf } from 'react-icons/io'
+import { DiOpensource } from 'react-icons/di'
+import { FaMapMarkedAlt } from 'react-icons/fa'
 
 import Triangles from '../Triangles'
+import styles from './About.module.css'
 
 const About = React.memo(() => (
     <section>
@@ -10,44 +14,51 @@ const About = React.memo(() => (
             Hi! I'm Samuel, an orienteer, system developer and open source
             enthusiast currently living in Stockholm, Sweden.
         </p>
-        <p>
-            I'm a creator who enjoys solving problems, learning and constantly
-            challenging my abilities. Another thing I really love is to share my
-            knowledge and help others. In fact, I think we're better off by{' '}
-            <a href="https://github.com/Greenheart">sharing our creations</a>.
-            This allows others to learn from our experience, and give a starting
-            point for new creations. Just look at the impact of open source
-            software in the past twenty years.
-        </p>
 
-        <p>
-            I currently focus on full stack JavaScript development, but I'm also
-            open to using other technology stacks. By programming regularly
-            since late 2013 and working with various technologies, I've come to
-            a point where I can take my general programming knowledge and apply
-            it to several different languages and environments. It all depends
-            on what is a good solution for the problem.
-        </p>
+        <div className={styles.topic}>
+            <p className={styles.icon}>
+                <IoIosCode />
+            </p>
+            <p>
+                I mostly work with full stack JavaScript, but I'm also
+                comfortable with other tech stacks. It all depends on what's a
+                good solution to the problem.
+            </p>
+        </div>
 
-        <p>
-            Since the late summer 2015, I've been involved in the learning
-            community <a href="https://freecodecamp.org/">freeCodeCamp</a>,
-            volunteering as a programming mentor, course content creator and
-            developer. This has not only taught me about software development or
-            managing large-scale projects, but also about how to effectively
-            communicate and work as a team. Because of this, I care about
-            creating a good team environment where people are encouraged to help
-            each other learn and grow.
-        </p>
+        <div className={styles.topic}>
+            <p className={styles.icon}>
+                <DiOpensource />
+            </p>
+            <p>
+                Open source enthusiast who enjoys sharing what I learn and help
+                others. Former core team member of the education non-profit{' '}
+                <a href="https://github.com/freeCodeCamp/freeCodeCamp">
+                    freeCodeCamp.org
+                </a>
+            </p>
+        </div>
 
-        <p>
-            As you might have guessed from the background image, another big
-            part of my life is orienteering. This is a sport that gives
-            challenging problem solving, good exercise and sometimes even
-            adventures! I practice regularly and like to explore how different
-            it can be to navigate in new terrain. Guess there couln't be a
-            better hobby for a developer, constantly exploring new code.
-        </p>
+        <div className={styles.topic}>
+            <p className={styles.icon}>
+                <IoIosLeaf />
+            </p>
+            <p>
+                Sustainability and environmental issues are key areas of my
+                work. There's no programming on a dead planet.
+            </p>
+        </div>
+
+        <div className={styles.topic}>
+            <p className={styles.icon}>
+                <FaMapMarkedAlt />
+            </p>
+            <p>
+                Orienteering - fun exercise while exploring new terrain. I guess
+                there couldn't be a better hobby as a developer, always
+                exploring new code.
+            </p>
+        </div>
 
         <Triangles color="var(--jet)" direction="bottom" />
     </section>
