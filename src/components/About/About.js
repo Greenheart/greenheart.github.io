964538945477113c5ea57a4272aff3c27d14bf58
Samuel Plumppu <greenheartmusic@gmail.com>
2019-07-29 21:39:21 +0200

    About section layout alternative 1.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,49 +15,52 @@ const About = React.memo(() => (
             enthusiast currently living in Stockholm, Sweden.
         </p>
 
-        <div className={styles.topic}>
-            <p className={styles.icon}>
-                <IoIosCode />
-            </p>
-            <p>
-                I mostly work with full stack JavaScript, but I'm also
-                comfortable with other tech stacks. It all depends on what's a
-                good solution to the problem.
-            </p>
-        </div>
+        <div className={styles.topics}>
+            <div className={styles.topic}>
+                <p className={styles.icon}>
+                    <IoIosCode />
+                </p>
+                <p>
+                    I mostly work with full stack JavaScript, but I'm also
+                    comfortable with other tech stacks. It all depends on what's
+                    a good solution to the problem.
+                </p>
+            </div>
 
-        <div className={styles.topic}>
-            <p className={styles.icon}>
-                <DiOpensource />
-            </p>
-            <p>
-                Open source enthusiast who enjoys sharing what I learn and help
-                others. Former core team member of the education non-profit{' '}
-                <a href="https://github.com/freeCodeCamp/freeCodeCamp">
-                    freeCodeCamp.org
-                </a>
-            </p>
-        </div>
+            <div className={styles.topic}>
+                <p className={styles.icon}>
+                    <DiOpensource />
+                </p>
+                <p>
+                    Open source enthusiast who enjoys sharing what I learn and
+                    help others. Former core team member of the education
+                    non-profit{' '}
+                    <a href="https://github.com/freeCodeCamp/freeCodeCamp">
+                        freeCodeCamp.org
+                    </a>
+                </p>
+            </div>
 
-        <div className={styles.topic}>
-            <p className={styles.icon}>
-                <IoIosLeaf />
-            </p>
-            <p>
-                Sustainability and environmental issues are key areas of my
-                work. There's no programming on a dead planet.
-            </p>
-        </div>
+            <div className={styles.topic}>
+                <p className={styles.icon}>
+                    <IoIosLeaf />
+                </p>
+                <p>
+                    Sustainability and environmental issues are key areas of my
+                    work. There's no programming on a dead planet.
+                </p>
+            </div>
 
-        <div className={styles.topic}>
-            <p className={styles.icon}>
-                <FaMapMarkedAlt />
-            </p>
-            <p>
-                Orienteering - fun exercise while exploring new terrain. I guess
-                there couldn't be a better hobby as a developer, always
-                exploring new code.
-            </p>
+            <div className={styles.topic}>
+                <p className={styles.icon}>
+                    <FaMapMarkedAlt />
+                </p>
+                <p>
+                    Orienteering - fun exercise while exploring new terrain. I
+                    guess there couldn't be a better hobby as a developer,
+                    always exploring new code.
+                </p>
+            </div>
         </div>
 
         <Triangles color="var(--jet)" direction="bottom" />
