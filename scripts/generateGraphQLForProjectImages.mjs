ebb2470e5f9148eccaf8b94babac5f5502bdf9f8
Samuel Plumppu <greenheartmusic@gmail.com>
2019-12-21 00:20:11 +0100

    Update script for generating project image GraphQL

--- scripts/generateGraphQLForProjectImages.mjs
+++ scripts/generateGraphQLForProjectImages.mjs
@@ -1,11 +1,17 @@
-import { projectImages } from '/src/components/Projects'
-
 /*
     Since Gatsby doesn't allow dynamic GraphQL-queries when fetching images at build time,
     this script can be used to generate the query manually, everytime project images change.
 
     Then copy the result back to the actual GraphQL query that's fetching the project images.
+
+    ## Usage:
+
+    1) Update projectImages in '/src/components/Projects/Projects.js' and copy the object to this file.
+    2) Run script: node --experimental-modules scripts/generateGraphQLForProjectImages.mjs
+    3) Paste resulting GraphQL query back into '/src/components/Projects/Projects.js'
 */
+
+const projectImages = {}
 
 const getGraphQLForImage = ({
     projectId,
@@ -15,14 +21,14 @@ const getGraphQLForImage = ({
 }) =>
     `
 ${projectId}: file(relativePath: { eq: "${imageName}" }) {
-childImageSharp {
-    fluid(quality: ${quality}, maxWidth: ${maxWidth}) {
-        ...GatsbyImageSharpFluid_withWebp
+    childImageSharp {
+        fluid(quality: ${quality}, maxWidth: ${maxWidth}) {
+            ...GatsbyImageSharpFluid_withWebp
+        }
     }
-}
 }`.trim()
 
-const getGraphQLForImages = (config = projectImages) => {
+export const getGraphQLForImages = (config = projectImages) => {
     return Object.entries(config).reduce(
         (queryString, [projectId, imageName]) =>
             queryString + '\n' + getGraphQLForImage({ projectId, imageName }),
@@ -31,4 +37,4 @@ const getGraphQLForImages = (config = projectImages) => {
 }
 
 // Copy the output from this call.
-getGraphQLForImages()
+console.log(getGraphQLForImages())
