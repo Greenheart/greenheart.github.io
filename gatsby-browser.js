9673b7b3ade763fe5010f6cfc86e69efad72e0f8
Samuel Plumppu <greenheartmusic@gmail.com>
2020-12-24 03:23:59 +0100

    Initial commit for 2021 website

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-exports.onClientEntry = () => {
-    // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
-    if (typeof window.IntersectionObserver === 'undefined') {
-        window.IntersectionObserver = import('intersection-observer')
-        console.log('# IntersectionObserver is polyfilled!')
-    }
-}
