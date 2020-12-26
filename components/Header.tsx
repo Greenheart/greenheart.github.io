6d8ab537f8e91e1a690f840ec8415b943a6e824a
Samuel Plumppu <greenheartmusic@gmail.com>
2020-12-27 00:34:46 +0100

    Improve header links and layout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,35 @@
+import Link from 'next/link'
+
 const Header = () => (
-    <header className="flex justify-between px-4 py-1 bg-white">
+    <header className="flex justify-between p-1 bg-white">
         <div className="flex w-md items-center space-x-4">
-            <img
-                src="/images/compass-icon-square.svg"
-                alt="Compass logo"
-                className="w-16 h-16"
-            />
-            <img
-                src="/images/sp-logo.svg"
-                alt="Samuel Plumppu name logo"
-                className="max-w-xs"
-            />
+            <Link href="/">
+                <a>
+                    <img
+                        src="/images/compass-icon-square.svg"
+                        alt="Compass logo"
+                        className="w-16 h-16"
+                    />
+                </a>
+            </Link>
+            <Link href="/">
+                <a>
+                    <img
+                        src="/images/sp-logo.svg"
+                        alt="Samuel Plumppu name logo"
+                        className="max-w-xs"
+                    />
+                </a>
+            </Link>
         </div>
-        <nav className="flex justify-between space-x-4">
-            <a className="flex items-center">About</a>
-            <a className="flex items-center">Blog</a>
-            <a className="flex items-center">Contact</a>
+        <nav className="flex justify-between">
+            <Link href="/about">
+                <a className="flex items-center px-4">About</a>
+            </Link>
+            <Link href="/blog">
+                <a className="flex items-center px-4">Blog</a>
+            </Link>
+            <a className="flex items-center px-4">Contact</a>
         </nav>
     </header>
 )
