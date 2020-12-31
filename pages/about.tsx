495b460d1e1275893c7276a0c5a349511971c722
Samuel Plumppu <greenheartmusic@gmail.com>
2020-12-31 13:19:57 +0100

    Add missing pages

---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout'
+
+function About() {
+    return <Layout variant="default">About me</Layout>
+}
+
+export default About
---
+++ pages/blog/index.tsx
@@ -0,0 +1,7 @@
+import Layout from '../../components/Layout'
+
+function Blog() {
+    return <Layout variant="default">List of all blog posts</Layout>
+}
+
+export default Blog
