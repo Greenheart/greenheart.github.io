d09eb0a19a33ade393f822dcfe0892a493539d54
Samuel Plumppu <greenheartmusic@gmail.com>
2021-05-22 19:14:25 +0200

    Add simple base layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,25 @@
-<script lang="ts">
+<script context="module" lang="ts">
     import '../global.css'
     import 'prism-themes/themes/prism-night-owl.css'
 </script>
 
-<slot />
+<script>
+    const links = [{ text: 'Blog', href: '/blog' }, { text: 'About', href: '/about' }]
+</script>
 
-<!-- TODO: Base layout for all pages -->
-<!-- TODO: Add navigation, footer -->
+<header class="flex flex-1 justify-between p-4">
+    <a href="/" sveltekit:prefetch class="font-semibold">Samuel Plumppu</a>
+    <nav class="flex space-x-4">
+        {#each links as link}
+            <a href={link.href} sveltekit:prefetch>{link.text}</a>
+        {/each}
+    </nav>
+</header>
+
+<div class="container mx-auto">
+    <slot />
+</div>
+
+<footer class="p-4 text-center mt-8">
+    Samuel Plumppu © 2015 - {new Date().getFullYear()}
+</footer>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -3,3 +3,5 @@
 
 <!-- TODO: Add /tags/index.svelte - List all tags and how many posts that exist with each, sort by most posts at the top -->
 <!-- TODO: Add /tags/[tag].svelte - List all blog posts with a specific tag, latest first -->
+
+<h1>Blog</h1>
