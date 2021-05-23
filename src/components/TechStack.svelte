2e5c60e47fac93a809b289cc0475233cf81e57d6
Samuel Plumppu <greenheartmusic@gmail.com>
2021-05-23 13:11:27 +0200

    Respect prefers-reduced-motion media query to disable animations

--- src/components/TechStack.svelte
+++ src/components/TechStack.svelte
@@ -2,6 +2,7 @@
     import { fade } from 'svelte/transition'
     import { flip } from 'svelte/animate'
     import { quintOut } from 'svelte/easing'
+    import MediaQuery from 'svelte-media-query'
 
     import type { TechStack } from '$lib/interfaces'
 
@@ -73,12 +74,30 @@
 
     <!-- Workaround to replace flexbox gap: https://gist.github.com/OliverJAsh/7f29d0fa1d35216ec681d2949c3fe8b7 -->
     <div class="flex flex-wrap mt-4 max-w-prose -ml-2 -mb-2 justify-center">
-        {#each tech[selected] as technology, index (technology)}
-            <span class="pl-2 pb-5" animate:flip={{ duration: 200, easing: quintOut }}>
-                <span class="p-2 bg-green-200" in:fade={{ delay: index * 35, duration: 200 }}>
-                    {technology}
-                </span>
-            </span>
-        {/each}
+        <MediaQuery query="(prefers-reduced-motion: reduce)" let:matches>
+            {#if matches}
+                {#each tech[selected] as technology (technology)}
+                    <span class="pl-2 pb-5">
+                        <span class="p-2 bg-green-200">
+                            {technology}
+                        </span>
+                    </span>
+                {/each}
+            {:else}
+                {#each tech[selected] as technology, index (technology)}
+                    <span
+                        class="pl-2 pb-5"
+                        animate:flip={{ duration: 200, easing: quintOut }}
+                    >
+                        <span
+                            class="p-2 bg-green-200"
+                            in:fade={{ delay: index * 35, duration: 200 }}
+                        >
+                            {technology}
+                        </span>
+                    </span>
+                {/each}
+            {/if}
+        </MediaQuery>
     </div>
 </section>
