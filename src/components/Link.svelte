d90a246e7cbb1ea91e42dd265c46d2d945fac944
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2021-07-20 11:15:18 +0200

    Trim combined classes

--- src/components/Link.svelte
+++ src/components/Link.svelte
@@ -2,7 +2,7 @@
     export let style = ''
     export let href = ''
 
-    const classes = [$$props.class ?? '', 'link'].join(' ')
+    const classes = [$$props.class ?? '', 'link'].join(' ').trim()
 </script>
 
 <a {href} {style} class={classes} {...$$props}>
