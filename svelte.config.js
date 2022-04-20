09794dd0ee1ab9bde9a9a4bd7c606008494335be
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-04-20 19:04:13 +0200

    Cleanup old config

--- svelte.config.js
+++ svelte.config.js
@@ -5,16 +5,9 @@ import { resolve } from 'path'
 
 import mdsvexConfig from './mdsvex.config.js'
 
-// Fix Tailwind CSS live reloading
-// Deatils: https://github.com/svelte-add/svelte-add/issues/67
-process.env.TAILWIND_MODE =
-    process.env.NODE_ENV === 'development' ? 'watch' : 'build'
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
     extensions: ['.svelte', ...mdsvexConfig.extensions],
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
     preprocess: [
         mdsvex(mdsvexConfig),
         preprocess({
