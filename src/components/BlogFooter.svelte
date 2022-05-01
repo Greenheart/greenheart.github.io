49f89512478fd8045e2a860b0e815a11ac38078d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-05-01 18:59:46 +0200

    Use separate component for BlogFooter

---
+++ src/components/BlogFooter.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" context="module">
+    import Link from './Link.svelte'
+</script>
+
+<section
+    class="p-4 bg-white shadow-lg rounded-md mx-auto mt-12 max-w-md text-center"
+>
+    <h2
+        class="text-xl xs:text-2xl md:text-3xl leading-none font-black tracking-tight mb-4"
+    >
+        Thank you for reading! 🌱
+    </h2>
+    <p class="lg:text-lg">
+        <Link href="/blog" class="p-[0.1rem]">Read more posts</Link>
+        or <Link href="/" class="p-[0.1rem]">learn more</Link> about me.
+    </p>
+</section>
--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -1,11 +1,12 @@
 <script context="module">
     import Tags from '$components/Tags.svelte'
+    import BlogFooter from '$components/BlogFooter.svelte'
     import { formatDate } from '$lib/formatDate'
     import { baseTitle } from '$lib/constants'
 
     // Override default components used to render posts: https://mdsvex.com/docs#custom-components
-    import Link from '$components/Link.svelte'
-    export { Link as a }
+    import { default as a } from '$components/Link.svelte'
+    export { a }
 
     import 'prism-themes/themes/prism-night-owl.css'
 </script>
@@ -13,6 +14,7 @@
 <script>
     // TODO: Update this layout to use TypeScript when mdsvex supports it.
     // See https://github.com/pngwn/MDsveX/issues/116
+
     // As a temporary fix, default values are used to describe the expected types
     export let title = ''
     export let date = ''
@@ -37,19 +39,7 @@
     <slot />
 </article>
 
-<section
-    class="p-4 bg-white shadow-lg rounded-md mx-auto mt-12 max-w-md text-center"
->
-    <h2
-        class="text-xl xs:text-2xl md:text-3xl leading-none font-black tracking-tight mb-4"
-    >
-        Thank you for reading! 🌱
-    </h2>
-    <p class="lg:text-lg">
-        <Link href="/blog" class="p-[0.1rem]">Read more posts</Link>
-        or <Link href="/" class="p-[0.1rem]">learn more</Link> about me.
-    </p>
-</section>
+<BlogFooter />
 
 <!-- IDEA: Add links to more posts at the bottom of every post -->
 <!-- IDEA: Add short info about me at the bottom of every post, linking to home/about for more details -->
