81039d8d8133bd780fb423022f26b6b5ce1ffdbb
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-05-11 01:01:38 +0200

    Improve postbuild

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -6,15 +6,13 @@ const getDirectories = async (source) =>
         .filter((dirent) => dirent.isDirectory())
         .map((dirent) => dirent.name)
 
-export const ALL_TALKS = await getDirectories(resolve('./static/talks'))
+export const ALL_TALKS = await getDirectories(resolve('static/talks'))
 
 const UNWANTED_FILES = ['.html', '__data.json']
 
 // Remove weird build artifacts caused by combining SvelteKit `static` assets with rendered pages
 await Promise.all(
     ALL_TALKS.flatMap((talk) =>
-        UNWANTED_FILES.map((file) =>
-            rm(resolve(`./build/talks/${talk}`, file)),
-        ),
+        UNWANTED_FILES.map((file) => rm(resolve(`build/talks/${talk}`, file))),
     ),
 )
