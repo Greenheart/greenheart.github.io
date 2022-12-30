b0c3b2146d47333062c6941c2f0316c6ba8aea79
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-30 21:57:01 +0100

    Cleanup

--- src/routes/talks/[slug]/+page.server.ts
+++ src/routes/talks/[slug]/+page.server.ts
@@ -7,19 +7,12 @@ export const prerender = false
 export const trailingSlash = 'always'
 
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ params: { slug }, setHeaders }) {
+export async function load({ params: { slug } }) {
     if (talks.includes(slug)) {
         const talk = await readFile(
             resolve(process.cwd(), 'static', 'talks', slug, 'index.html'),
             { encoding: 'utf-8' },
         )
-
-        // This is important for SSR apps, but in this case.
-        // However, in this case we could remove it since we're prerendering the entire site
-        // and thus have no use for server features.
-        setHeaders({
-            location: `/talks/${slug}/`,
-        })
 
         if (talk) return { talk }
     }
