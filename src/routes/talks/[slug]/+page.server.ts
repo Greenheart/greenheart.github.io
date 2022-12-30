b2c1ee73054e97591dfbd7f774b0f80acc17d13e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-30 21:56:35 +0100

    Clarify behavior

--- src/routes/talks/[slug]/+page.server.ts
+++ src/routes/talks/[slug]/+page.server.ts
@@ -3,6 +3,9 @@ import { resolve } from 'path'
 import talks from '$lib/talks'
 import { error } from '@sveltejs/kit'
 
+export const prerender = false
+export const trailingSlash = 'always'
+
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params: { slug }, setHeaders }) {
     if (talks.includes(slug)) {
@@ -11,6 +14,9 @@ export async function load({ params: { slug }, setHeaders }) {
             { encoding: 'utf-8' },
         )
 
+        // This is important for SSR apps, but in this case.
+        // However, in this case we could remove it since we're prerendering the entire site
+        // and thus have no use for server features.
         setHeaders({
             location: `/talks/${slug}/`,
         })
