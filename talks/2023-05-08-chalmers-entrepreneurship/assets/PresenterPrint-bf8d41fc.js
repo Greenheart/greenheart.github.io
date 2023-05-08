49270fa176b117f3b2f55e74683a7d4ec8f31937
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-05-08 10:20:09 +0200

    Updates

--- talks/2023-05-08-chalmers-entrepreneurship/assets/PresenterPrint-bf8d41fc.js
+++ talks/2023-05-08-chalmers-entrepreneurship/assets/PresenterPrint-bf8d41fc.js
@@ -1,4 +1,4 @@
-import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as o,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as N,m as k,p as P,q as S,_ as w}from"./index-31046857.js";import{N as L}from"./NoteDisplay-ecf6858f.js";const V={class:"m-4"},j={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),A={key:0,class:"border-gray-400/50 mb-8"},E=_({__name:"PresenterPrint",setup(F){d(p),h(`
+import{d as _,i as d,a as p,u as h,b as u,c as m,e as f,f as n,g as t,t as o,h as a,F as g,r as v,n as x,j as y,o as r,k as b,l as N,m as k,p as P,q as S,_ as w}from"./index-e16601ed.js";import{N as L}from"./NoteDisplay-0a5a3f5d.js";const V={class:"m-4"},j={class:"mb-10"},T={class:"text-4xl font-bold mt-2"},B={class:"opacity-50"},C={class:"text-lg"},D={class:"font-bold flex gap-2"},H={class:"opacity-50"},z=t("div",{class:"flex-auto"},null,-1),A={key:0,class:"border-gray-400/50 mb-8"},E=_({__name:"PresenterPrint",setup(F){d(p),h(`
 @page {
   size: A4;
   margin-top: 1.5cm;
