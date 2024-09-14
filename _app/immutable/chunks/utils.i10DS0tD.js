9bbd1c83820006b34a289e76df984b226c7a03f5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2024-09-14 13:32:59 +0200

    Updates

---
+++ _app/immutable/chunks/utils.i10DS0tD.js
@@ -0,0 +1,1 @@
+const o=(...t)=>t.filter(Boolean).join(" ").trim(),e=new Intl.DateTimeFormat("en-GB",{year:"numeric",month:"long",day:"numeric"});function a(t){return e.format(new Date(t))}function r(t){return function(n){t&&t.call(this,n),t=null}}export{o as c,a as f,r as o};
