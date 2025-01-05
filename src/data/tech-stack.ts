e3414213ccd5d2e8dbdf4d96b5d065b53bc07ce3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-01-05 11:59:49 +0100

    Update tech stack

--- src/data/tech-stack.ts
+++ src/data/tech-stack.ts
@@ -1,22 +1,27 @@
 import type { TechStack } from '$lib/interfaces'
 
 const tech: TechStack = {
+    Learning: ['Kubernetes', 'Local-first', 'CRDT'],
     Current: [
         'TypeScript',
+        'Python',
         'Node.js',
         'Svelte',
         'React',
+        'Fastify',
         'Astro',
         'SvelteKit',
         'Tailwind CSS',
         'Docker',
+        'Podman',
         'PostgreSQL',
         'SQLite',
         'Redis',
         'Prisma',
+        'BullMQ',
+        'Drizzle ORM',
     ],
     Past: [
-        'Python',
         'C#',
         'Elixir',
         'PHP',
@@ -30,8 +35,9 @@ const tech: TechStack = {
         'Mongo DB',
         'GraphQL',
         'Phaser',
+        'Express',
+        'Elasticsearch',
     ],
-    Learning: ['Fastify', 'BullMQ', 'Kubernetes'],
 }
 
 export default tech
