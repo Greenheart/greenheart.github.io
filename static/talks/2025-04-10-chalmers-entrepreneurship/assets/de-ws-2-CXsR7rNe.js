14cbaac4f28321617ab695ea8e2a00105d10b932
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-04-14 14:24:59 +0200

    Publish new talk

---
+++ static/talks/2025-04-10-chalmers-entrepreneurship/assets/de-ws-2-CXsR7rNe.js
@@ -0,0 +1,1 @@
+const e="/talks/2025-04-10-chalmers-entrepreneurship/images/de-ws-2.png";export{e as _};
---
+++ static/talks/2025-04-10-chalmers-entrepreneurship/assets/deep-design-of-business-BsDNvwkl.js
@@ -0,0 +1,1 @@
+const e="/talks/2025-04-10-chalmers-entrepreneurship/images/deep-design-of-business.jpg";export{e as _};
