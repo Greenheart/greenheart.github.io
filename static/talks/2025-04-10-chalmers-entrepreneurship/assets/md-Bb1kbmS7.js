a98a47cc00d5d607a056e475928dcb78b18123db
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-05-01 09:06:45 +0200

    Update talks

---
+++ static/talks/2025-04-10-chalmers-entrepreneurship/assets/md-Bb1kbmS7.js
@@ -0,0 +1,12 @@
+import{_ as l}from"./slidev/VClick-CJYhjoVZ.js";import{b as p,o as u,w as o,g as n,e,i as m,v as f,x as d,S as r}from"./modules/vue-DI_sQAFs.js";import{_}from"./slidev/image-right.vue_vue_type_script_setup_true_lang-DQKk8DeS.js";import{u as c,f as k}from"./slidev/context-0inonGp6.js";import"./index-CdNzNO2l.js";import"./modules/shiki-DmDJ2_-c.js";import"./slidev/VClicks-Bbo93bFb.js";import"./slidev/layoutHelper-DDDoriTz.js";const P={__name:"slides.md__slidev_7",setup(w){const{$clicksContext:a,$frontmatter:i}=c();return a.setup(),(v,t)=>{const s=l;return u(),p(_,f(d(r(k)(r(i),6))),{default:o(()=>[t[2]||(t[2]=n("h1",null,"Discuss in pairs:",-1)),t[3]||(t[3]=n("p",null,"Take turns and ask follow-up questions. 5 min per person.",-1)),e(s,null,{default:o(()=>t[0]||(t[0]=[n("h2",{class:"pt-8"},"1. What's your vision for a sustainable future?",-1)])),_:1}),e(s,null,{default:o(()=>t[1]||(t[1]=[n("h2",{class:"pt-8"},"2. Which skills and perspectives do you want to contribute with?",-1)])),_:1}),m(`
+
+
+What if...
+
+
+- an economy that prioritised wellbeing for all within the means of the living planet?
+- 4 day work week
+- 
+
+
+`)]),_:1},16)}}};export{P as default};
