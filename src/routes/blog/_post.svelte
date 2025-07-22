655977fb8e092bf8f015b0cdd17ca4ee173589e1
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-07-22 11:35:58 +0200

    WIP: migrate tailwind-plugin-typography from js config to css config

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -39,7 +39,7 @@
 <h1 class="mx-auto text-4xl font-black lg:text-5xl">{title}</h1>
 
 <article
-    class="prose lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-blockquote:bg-white prose-blockquote:py-1 prose-blockquote:rounded-md mx-auto mt-4 text-base"
+    class="prose prose-hr:border-ming lg:prose-lg xl:prose-xl 2xl:prose-2xl prose-blockquote:text-black prose-strong:text-black prose-blockquote:bg-white prose-blockquote:py-1 prose-blockquote:rounded-md prose-headings:text-black mx-auto mt-4 max-w-[75ch] text-base text-black marker:text-black"
 >
     <time datetime={date}>{formatDate(date)}</time>
     <Tags {tags} class="my-2" />
