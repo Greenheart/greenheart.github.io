af52e48f1de8d66b4704a6726a4c14ce9c8958ca
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-08 10:37:26 +0200

    Avoid hiding the error message

--- src/routes/blog/[...catchall]/+page.ts
+++ src/routes/blog/[...catchall]/+page.ts
@@ -1,4 +1,3 @@
-import { error } from '@sveltejs/kit'
 import type { MetaTagsProps } from 'svelte-meta-tags'
 
 import { getPost, listPosts } from '$lib/posts'
@@ -6,27 +5,22 @@ import { getPost, listPosts } from '$lib/posts'
 export const load = async ({ params }) => {
     const slug = params.catchall
 
-    try {
-        const post = await getPost(slug)
-        const title = post.title
+    const post = await getPost(slug)
+    const title = post.title
 
-        const posts = await listPosts()
-        const current = posts.findIndex((post) => post.slug === slug)
-        const older = posts[current + 1] ?? null
-        const newer = posts[current - 1] ?? null
+    const posts = await listPosts()
+    const current = posts.findIndex((post) => post.slug === slug)
+    const older = posts[current + 1] ?? null
+    const newer = posts[current - 1] ?? null
 
-        return {
-            post,
-            otherPostsCount: posts.length - 1,
-            older,
-            newer,
-            pageMetaTags: Object.freeze({
-                title,
-                openGraph: { title },
-            }) satisfies MetaTagsProps,
-        }
-    } catch (e) {
-        console.error(e)
-        throw error(404, `No corresponding file found for the slug "${slug}"`)
+    return {
+        post,
+        otherPostsCount: posts.length - 1,
+        older,
+        newer,
+        pageMetaTags: Object.freeze({
+            title,
+            openGraph: { title },
+        }) satisfies MetaTagsProps,
     }
 }
