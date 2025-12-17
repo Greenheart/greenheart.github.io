997107cdaf9548fda3d70ab05a547325289b3794
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-12-17 17:26:39 +0100

    Render feed content to HTML strings
    - Remove CSS-based syntax highlighting from the rendered HTML
    - Remove comments since the feed content doesn't need to be hydratable
      by SvelteKit

--- src/lib/feed.ts
+++ src/lib/feed.ts
@@ -1,10 +1,35 @@
 import { Feed } from 'feed'
+import { render } from 'svelte/server'
+import type { Component } from 'svelte'
+
 import { SITE_DESCRIPTION, SITE_URL } from '$lib/constants'
 import { getAllPosts, type BlogPost } from '$lib/posts'
 import { getPostUpdatedAtFromGit } from './post-details.remote'
 
 const normalizedURL = SITE_URL.endsWith('/') ? SITE_URL : SITE_URL + '/'
 const samuel = { name: 'Samuel Plumppu', link: normalizedURL }
+
+/**
+ * Turn a `svelte-markdoc` document into a clean HTML string
+ *
+ * @param component Svelte component with Markdoc content.
+ * @returns HTML string
+ */
+async function renderSvelteMarkdocToHTML(component: Component) {
+    const html = (await render(component)).body
+    return (
+        html
+            // Remove CSS-based syntax highlighting since this is handled by the feed readers.
+            // IDEA: Add the code language as an attribute/class to the <pre>-element
+            // so maybe some feed readers could render syntax highlighting.
+            .replaceAll(/<pre[^>]*>/gm, '<pre>')
+            // Simplify markup by replacing code <span> elements with the plain text, formatted content
+            .replaceAll(/<span[^>]*>/gm, '')
+            .replaceAll(/<\/span>/gm, '')
+            // Remove all HTML comments, since we don't plan to hydrate this HTML as an SvelteKit app.
+            .replaceAll(/<!--([\s\S]*?)-->/gm, '')
+    )
+}
 
 export async function generateFeed() {
     const posts = await getAllPosts()
@@ -26,8 +51,8 @@ export async function generateFeed() {
 
     for (const post of posts) {
         const postURL = `${SITE_URL}/blog/${post.slug}`
-        // TODO: Render markdoc content as HTML
         const updatedAt = await getPostUpdatedAtFromGit(post.slug)
+        const content = await renderSvelteMarkdocToHTML(post.Content)
 
         feed.addItem({
             published: post.publishedAt,
@@ -35,7 +60,7 @@ export async function generateFeed() {
             date: updatedAt ?? post.publishedAt,
             title: post.title,
             link: postURL,
-            // content: ``,
+            content,
             id: postURL,
         })
 
