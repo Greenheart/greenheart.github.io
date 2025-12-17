1e83f402f7e02b931edee09de478d2e58e00ed26
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-12-17 19:09:31 +0100

    Add feed subscription links to page footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     import { MASTODON_URL } from '$lib/constants'
     import EncryptedEmail from './EncryptedEmail.svelte'
+    import SubscribeLinks from './SubscribeLinks.svelte'
 </script>
 
-<footer class="container mx-auto max-w-6xl px-4 pb-4 text-center font-light">
+<footer class="container mx-auto max-w-6xl px-4 pb-8 text-center">
     <hr class="border-ming mx-auto my-16 max-w-sm" id="contact" />
     <h2
         class="xs:text-2xl mb-6 text-xl leading-none font-black tracking-tight text-balance sm:text-3xl lg:text-4xl"
@@ -11,13 +12,6 @@
         Let's co-create a sustainable future!
     </h2>
 
-    <!-- IDEA: Update EncryptedEmail to work with a encrypted JSON payload, and then display EncryptedContact info (rename component and refactor) -->
-
-    <!--
-Contact securely
-- or -
-Show email
--->
     <EncryptedEmail />
 
     <p class="mt-8">© 2015 - {new Date().getFullYear()} Samuel Plumppu</p>
@@ -64,4 +58,6 @@ Show email
             <img src="/images/linkedin.svg" alt="LinkedIn" class="size-8" />
         </a>
     </div>
+
+    <SubscribeLinks />
 </footer>
