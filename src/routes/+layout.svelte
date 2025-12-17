4b5ae8263d66189d1ac0039a835c57aa3f3ea277
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-12-17 22:14:07 +0100

    Fix layout for mobile breakpoint

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,7 +27,7 @@
 </svelte:head>
 
 <header
-    class="mx-auto flex max-w-6xl flex-1 flex-wrap justify-between gap-x-2 gap-y-4 p-4"
+    class="xs:text-base 2xs:justify-between mx-auto flex max-w-6xl flex-1 flex-wrap justify-center gap-x-2 gap-y-4 p-4 text-sm"
 >
     <Link href="/">Samuel Plumppu</Link>
     <nav class="xs:space-x-4 flex space-x-2">
