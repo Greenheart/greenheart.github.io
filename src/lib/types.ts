c71065a4ccd71e91cc6e56c1d2bfa077a3634866
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-07 19:41:38 +0100

    Add helper type `RequiredProperties` together with a new usage example.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,35 @@
 export type { BlogPost } from '$lib/posts'
+export type { Project } from '$lib/projects'
 
 export type TechStack = {
     Current: string[]
     Past: string[]
     Learning: string[]
 }
+
+/**
+ * Mark properties P as required. Useful to override only a few properties.
+ *
+ * @example
+ * ```ts
+ * type RunningShoes = {
+ *   color: string
+ *   size?: string
+ *   comfortable?: boolean
+ * }
+ * type RunningShoesWithSize = RequiredProperties<RunningShoes, 'size'>
+ *
+ * // Works well: `size` is defined
+ * const favorites: RunningShoesWithSize = {
+ *   color: 'black',
+ *   size: 'perfect'
+ * }
+ * // Error: Missing required property `size`
+ * const barelyUsed: RunningShoesWithSize = {
+ *   color: 'darkgray',
+ *   comfortable: false
+ * }
+ * ```
+ */
+export type RequiredProperties<T, P extends keyof T> = Omit<T, P> &
+    Required<Pick<T, P>>
