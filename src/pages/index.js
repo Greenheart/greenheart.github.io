b477ea8665aa0c723ffbde4093b0483a846eea72
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-11 02:33:34 +0100

    Fix compatibility with case-insensitive file systems

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import About from '../components/About'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 import Contact from '../components/Contact'
-import SEO from '../components/SEO'
+import SEO from '../components/seo'
 
 const IndexPage = () => {
     return (
