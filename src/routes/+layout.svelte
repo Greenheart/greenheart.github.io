93227b7eb5a52a4d4de254d53bb9f27b7cd08996
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-11 01:03:02 +0100

    Add theme toggle

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
     import { deepMerge, MetaTags } from 'svelte-meta-tags'
 
     import { page } from '$app/state'
+    import { browser } from '$app/environment'
     import Link from '$components/Link.svelte'
     import Footer from '$components/Footer.svelte'
 
@@ -19,6 +20,51 @@
     let metaTags = $derived(
         deepMerge(data.baseMetaTags, page.data.pageMetaTags ?? {}),
     )
+
+    const themes = ['dark', 'light', 'system'] as const
+
+    function getTheme() {
+        debugger
+        if (!browser) return 'system'
+
+        if (localStorage.theme) {
+            return localStorage.theme
+        }
+
+        if (window.matchMedia('(prefers-color-scheme: dark)')) {
+            return 'dark'
+        }
+
+        return 'light'
+    }
+
+    function setTheme(theme: (typeof themes)[number]) {
+        if (theme === 'system') {
+            delete localStorage.theme
+        } else {
+            localStorage.theme = theme
+        }
+        document.documentElement.classList.toggle(
+            'dark',
+            localStorage.theme === 'dark' ||
+                (!('theme' in localStorage) &&
+                    window.matchMedia('(prefers-color-scheme: dark)').matches),
+        )
+        currentTheme = theme
+    }
+
+    let currentTheme = $state<(typeof themes)[number]>(getTheme())
+    let nextTheme = $derived(
+        themes[(themes.indexOf(currentTheme) + 1) % themes.length],
+    )
+    $inspect({
+        localStorage:
+            typeof localStorage !== 'undefined'
+                ? localStorage.theme
+                : undefined,
+        currentTheme,
+        nextTheme,
+    })
 </script>
 
 <MetaTags {...metaTags} />
@@ -42,6 +88,18 @@
         {#each links as link}
             <Link href={link.href}>{link.text}</Link>
         {/each}
+        <button
+            title={`Toggle theme from ${currentTheme} to ${nextTheme}`}
+            onclick={() => setTheme(nextTheme)}
+            class="cursor-pointer"
+        >
+            <img
+                src="/images/theme.svg"
+                alt="Theme toggle icon"
+                class="size-5 dark:invert"
+                fetchpriority="high"
+            />
+        </button>
     </nav>
 </header>
 
