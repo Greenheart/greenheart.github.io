37f6551ec186ca2b034ccc964d4fc3df9b6c68fa
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-01-16 22:52:12 +0100

    Add script to automatically update the tech skills based on the source of truth. This keeps the typescript definitions in sync with the actual data.
    This gains the benefits of type safety and spell checking, while
    avoiding the issue of circular references in TypeScript

---
+++ scripts/update-skills.ts
@@ -0,0 +1,26 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+
+import { TECH_SKILLS } from '../src/data/tech-skills.ts'
+
+if (import.meta.main) {
+    const path = resolve(import.meta.dirname, '../src/data/tech-skills.ts')
+    const rawFile = await readFile(path, 'utf-8')
+
+    const allSkills = Object.values(TECH_SKILLS).flatMap((category) =>
+        category.map((skill) => skill[0]),
+    )
+
+    const oldSkills = rawFile.match(
+        /export const ALL_TECH_SKILLS = \[\s*([\s\S]*)\n\] as const/,
+    )![1]
+
+    await writeFile(
+        path,
+        rawFile.replace(
+            oldSkills,
+            allSkills.map((skill) => `'${skill}'`).join(',\n    '),
+        ),
+        'utf-8',
+    )
+}
