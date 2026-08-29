3fddcc0976a24becbc8f991d34f2f1c2d24a7cfb
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2026-08-29 21:50:18 +0200

    Migrate to new config format and remove unused vitePreprocess

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,33 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import tailwind from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
+import adapter from '@sveltejs/adapter-static'
+import { markdocPreprocess } from 'markdoc-svelte'
+import { resolve } from 'node:path'
+
+import { markdocConfig } from './src/lib/markdoc.ts'
 
 export default defineConfig({
-    plugins: [enhancedImages(), sveltekit(), tailwind()],
+    plugins: [
+        enhancedImages(),
+        sveltekit({
+            extensions: ['.svelte', '.md', '.mdoc'],
+            preprocess: markdocPreprocess(markdocConfig),
+            adapter: adapter(),
+            alias: {
+                $components: resolve('./src/components'),
+                $data: resolve('./src/data'),
+                $assets: resolve('./src/assets'),
+            },
+            experimental: {
+                remoteFunctions: true,
+            },
+            compilerOptions: {
+                experimental: {
+                    async: true,
+                },
+            },
+        }),
+        tailwind(),
+    ],
 })
